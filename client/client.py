38b69f41f0e6f05a9c28f626366f280d2be17130
Stephen Lin <s.lin@alumni.ubc.ca>
2019-06-20 15:17:45 -0700

    added windows compatibility

--- client/client.py
+++ client/client.py
@@ -1,50 +1,6 @@
-# import socket
-# hostip = 'localhost'
-# hostport = 8089
-#
-# class client_qontrol():
-#     def __init__(self):
-#         clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         clientsocket.connect((hostip, hostport))
-#         clientsocket.send(bytes('Client Connected.', 'UTF-8'))
-#         self.do()
-#
-#     def recognized_command(self,command):
-#         clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         clientsocket.connect((hostip, hostport))
-#         clientsocket.send(bytes(command, 'UTF-8'))
-#         response = clientsocket.recv(1024)
-#         print(response.decode("UTF-8"))
-#         return response.decode("UTF-8")
-#
-#     def do(self):
-#         while True:
-#             commandRAW = input('[Client]: ')
-#             command = commandRAW.split()[0]
-#
-#             if command == 'help':
-#                 print("Possible Commands: help, setvoltage, setcurrent, getvoltage, getcurrent.")
-#                 continue
-#             elif command == 'test_command' \
-#             or command == 'setvoltage'\
-#             or command == 'getvoltage'\
-#             or command == 'setcurrent'\
-#             or command == 'getcurrent'\
-#             :
-#                 self.recognized_command(commandRAW)
-#
-#             elif command == 'exit':
-#                 print("Exited.")
-#                 break
-#             else:
-#                 print("Command Not Recognized. Try: help")
-#                 continue
-#
-#
-#
-# #x = client_qontrol()
-# #recognized_command('test_command')
-
+##
+#Author: Stephen
+##
 import socket
 import select
 import sys, os
@@ -58,8 +14,6 @@ def connect(hostip=hostip,hostport=hostport):
     IP_address = hostip
     Port = hostport
     server.connect((IP_address, Port))
-    #message = socks.recv(2048)
-    #print("[Server]: "+message.decode("UTF-8"))
     return server
 
 def terminal_mode():
@@ -105,8 +59,37 @@ def send_command(message):
     #print(response.decode("UTF-8"))
     return response.decode("UTF-8")
 
+def WIN_read_socket(server):
+    sockets_list = [server] #ONLY THIS IS DIFFERENT
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    while True:
+        for socks in read_sockets:
+            if socks == server:
+                # maintains a list of possible input streams
+                message = socks.recv(2048)
+                sys.stdout.write("[Server]: "+message.decode("UTF-8"))
+                sys.stdout.write("\n\n[You]: ")
+                sys.stdout.flush()
+    print('Connection Closed.')
+    server.close()
+    
+def WIN_write_socket(server):
+    while True:
+        message = sys.stdin.readline()
+        if message == 'exit':
+            return
+        else:
+            server.send(bytes(message, 'UTF-8'))
+        
 try:
     if str(sys.argv[1]) == 'terminal':
-        terminal_mode()
+        if str(sys.argv[2]) == 'windows':
+            from threading import *
+            server = connect()
+            server.send(bytes('Connected via Terminal. Hello!\n','UTF-8'))
+            Thread(target=WIN_read_socket, args=(server,)).start()  
+            Thread(target=WIN_write_socket, args=(server,)).start()  
+        else:
+            terminal_mode()
 except:
     pass
